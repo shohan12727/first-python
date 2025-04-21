b291d46339237bd470553dfa4b1f37370360321a
shohan12727 <aishohan001@gmail.com>
2025-04-22 03:08:19 +0600

    updated

--- index.py
+++ index.py
@@ -1,2 +1,32 @@
-print("Fuck pal")
-print("Fuck cse")
+# print("Fuck pal")
+# print("Fuck cse")
+# import sys
+# print(sys.version)
+# import sys
+# print(sys.verison)
+# import sys     
+# print(sys.version)
+# if 5 > 2 :
+#   print("five is greater than two")
+
+# y = "hello shohan"
+# print(y)
+
+# x = float(3)
+# print(x)
+
+# x = str(5)
+# print(x)
+
+
+v = "shohan"
+print(type(v))
+
+
+
+
+
+
+
+
+
