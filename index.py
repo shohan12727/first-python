f210ce995e5172c77903ec84208d2ff417047cac
shohan12727 <aishohan001@gmail.com>
2025-04-22 09:51:47 +0600

    variable done

--- index.py
+++ index.py
@@ -1,22 +1,22 @@
-# print("Fuck pal")
-# print("Fuck cse")
-# import sys
-# print(sys.version)
-# import sys
-# print(sys.verison)
-# import sys     
-# print(sys.version)
-# if 5 > 2 :
-#   print("five is greater than two")
+print("Fuck pal")
+print("Fuck cse")
+import sys
+print(sys.version)
+import sys
+print(sys.verison)
+import sys     
+print(sys.version)
+if 5 > 2 :
+  print("five is greater than two")
 
-# y = "hello shohan"
-# print(y)
+y = "hello shohan"
+print(y)
 
-# x = float(3)
-# print(x)
+x = float(3)
+print(x)
 
-# x = str(5)
-# print(x)
+x = str(5)
+print(x)
 
 
 v = "shohan"
---
+++ multivar.py
@@ -0,0 +1,50 @@
+x=y=z="shohan"
+print(x)
+print(y)
+print(z)
+
+
+fruits= ["apple", "banana", "cherry"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+
+x=y=z = "king shohan"
+print(x)
+print(y)
+print(z)
+
+x= "shohan"
+y="is"
+z="a"
+a="boy"
+print(x,y,z,a)
+print(x+ y+z +a)
+
+a= 9
+b= "limon"
+print(a,b)
+
+
+print(5+10)
+
+x= "awesome"
+def myFucntion() :
+    print("python is " + x)
+myFucntion()
+
+def additon (a,b) :
+    print(a+b)
+additon(44,4)
+
+def multiply(x,y):
+    print(x*y)
+multiply(5,10)
+
+def division(v,b):
+    print(v/b)
+
+division(100,5)
+
+
