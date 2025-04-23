ba079b5f9ac6463667183ac9bd4b634bce43e877
shohan12727 <aishohan001@gmail.com>
2025-04-23 23:55:51 +0600

    casting

---
+++ casting.py
@@ -0,0 +1,20 @@
+# x= int(1)
+
+# y= int(2.5)
+
+# z=("5")
+
+# print(x)
+# print(y)
+# print(z)
+
+# a= float("5")
+# print(a)
+
+print('abid molla')
+print("simanto saha")
+
+
+p="lorem ipsum"
+print(p)
+
