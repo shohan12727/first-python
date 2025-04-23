3a0e270b5450dd850d40d447ad1aa18e05b4bbed
shohan12727 <aishohan001@gmail.com>
2025-04-24 00:51:50 +0600

    concatenate done

--- concatenate.py
+++ concatenate.py
@@ -1,9 +1,23 @@
-a = "hello"
-b = " bangladesh"
-c = a+b
-print(c)
+# a = "hello"
+# b = " bangladesh"
+# c = a+b
+# print(c)
 
-x = "I love"
-y = "bangladesh"
-v= x+" "+y
-print(v)
+# x = "I love"
+# y = "bangladesh"
+# v= x+" "+y
+# print(v)
+
+
+age = 63
+txt = f"hi my name is jhon. i am {age} years old"
+print(txt)
+
+price = 2500
+statement = f"cost of this product is {price}"
+print(statement)
+
+
+price2 = 56
+text = f"the price is {price2:2f} dollers"
+print(text)
